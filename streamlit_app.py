7a891677b0136c824f532f707fd7324b91ddd717
Jordi <jordipuig37@gmail.com>
2023-11-01 11:12:59 +0100

    Update streamlit_app.py
    Added data from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Parents New Healty Diner")
 
@@ -22,3 +23,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+# Get data from the API
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
